ef079038774032342119d3830affecf87d4eeebf
mix23456 <mixsax2@gmail.com>
2023-10-05 23:30:39 +0700

    node

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ponlawat2001.github.io",
+  base: "https://ponlawat2001.github.io/",
   plugins: [react()],
 });

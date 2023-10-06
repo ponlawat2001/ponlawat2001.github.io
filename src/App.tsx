97c104b76e47a582387013d1b767eb0e57423867
mix23456 <mixsax2@gmail.com>
2023-10-06 18:38:22 +0700

    update contact

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import React from "react";
 import logo from "./assets/icon.png";
+import faceicon from "./assets/facebook.png";
+import disicon from "./assets/discord.png";
+import linkinicon from "./assets/linkedin.png";
+import giticon from "./assets/github-sign.png";
+
 function App() {
   const [screenSize, setScreenSize] = useState(getCurrentDimension());
 
@@ -36,11 +41,50 @@ function App() {
               <p className="des">CONTACT ME</p>
             </div>
             <div className="card-back">
-              <p className="title-back">Ponlawat Chatkaew</p>
-              <p className="des-back">Computer engineer</p>
-              <a href="https://github.com/ponlawat2001" target="blank">
-                Github
-              </a>
+              <div className="card-back-inner-left">
+                <p className="contact">
+                  <a
+                    href="https://www.linkedin.com/in/ponlawat-chatkaew-11491627a"
+                    target="blank"
+                  >
+                    <div>
+                      <img src={linkinicon} width={16} />
+                      Linkin
+                    </div>
+                  </a>
+                  <a href="https://github.com/ponlawat2001" target="blank">
+                    <div>
+                      <img src={giticon} width={16} />
+                      Github
+                    </div>
+                  </a>
+                  <a
+                    href="https://discord.com/users/mixponlawat"
+                    target="blank"
+                  >
+                    <div>
+                      <img src={disicon} width={16} />
+                      Discord
+                    </div>
+                  </a>
+                  <a
+                    href="https://www.facebook.com/mixzaponlawat"
+                    target="blank"
+                  >
+                    <div>
+                      <img src={faceicon} width={16} />
+                      Facebook
+                    </div>
+                  </a>
+                </p>
+              </div>
+
+              <div className="card-back-inner">
+                <p className="title-back">Ponlawat</p>
+                <p className="title-back">Chatkaew</p>
+                <p className="des-back">Computer engineer</p>
+                <p className="solid" />
+              </div>
             </div>
           </div>
         </div>
